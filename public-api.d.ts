dd64b19fb0df4c932b814838cb18d3b5e9b727e7
ansarishoab <shoab.8055@gmail.com>
2024-10-07 13:27:06 +0530

    changes

---
+++ public-api.d.ts
@@ -0,0 +1,4 @@
+export * from './lib/components/fn-button/fn-button.component';
+export * from './lib/components/fn-input/fn-input.component';
+export * from './lib/my-lib.service';
+export * from './lib/my-lib.component';
